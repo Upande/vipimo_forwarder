6bee72b84e0bbb53fb30e05a0b65f77e3726bb7e
Brian Onang'o <surgbc@gmail.com>
2017-10-04 09:28:31 +0300

    Sync time

--- start.sh
+++ start.sh
@@ -3,4 +3,5 @@
 cd /root/kcs_forwarder/
 # #check for updates when starting
 git pull	#update first, just in case things are broken and it fails to start completely
+date -s "$(wget -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f5-8)Z"
 node app.js

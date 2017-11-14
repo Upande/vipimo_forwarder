c53b558912fa75e8438dc5c653222cebf15794cb
Brian Onang'o <surgbc@gmail.com>
2017-11-14 07:55:32 +0300

    signalstrength

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 cd /root/kcs_forwarder/
 # #check for updates when starting
-yarn add string_decoder
+yarn add  node-html-encoder
 git pull	#update first, just in case things are broken and it fails to start completely
 date -s "$(wget -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f5-8)Z"
 node app.js

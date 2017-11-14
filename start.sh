577258793852ba0062345d30acf2017d500d6312
root <root@lorank8.(none)>
2017-11-14 05:40:04 +0000

    signalstrength

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 
 cd /root/kcs_forwarder/
 # #check for updates when starting
+yarn add  node-html-encoder
 yarn add string_decoder
 git pull	#update first, just in case things are broken and it fails to start completely
 date -s "$(wget -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f5-8)Z"

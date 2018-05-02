af295259682b640b21bf86f352faaad0f668aea6
Brian Onang'o <surgbc@gmail.com>
2018-05-02 04:28:17 +0000

    Added RTC

--- checkInternet.sh
+++ checkInternet.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+function setFromRTC()
+{
+        sleep 15
+        echo ds1307 0x68 > /sys/class/i2c-adapter/i2c-2/new_device
+        hwclock -s -f /dev/rtc1
+        hwclock -w
+}
+setFromRTC
 
 checktime=2	 #time in minutes after which to check for connection
 restarttime=12 #time after which to restart if no connection
@@ -37,6 +45,7 @@ function connectionCameBack? () {
 function setdate()
 {
 	date -s "$(wget -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f5-8)Z"
+	setFromRTC #set from RTC if RTC exists
 }
 
 function reversetunnel(){
